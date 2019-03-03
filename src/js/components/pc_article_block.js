8e216ca35e94d99c513e7bba207b7900e4faec1a
xbmchina <1131663064@163.com>
2019-03-03 16:55:56 +0800

    '右侧相关链接栏'

---
+++ src/js/components/pc_article_block.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Card,Divider,Icon } from 'antd';
+
+import { Link } from "react-router-dom";
+
+export default class PCArticleBlock extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            news: ''
+        };
+    }
+
+    componentWillMount() {
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=" + this.props.type + "&count=" + this.props.count, myFetchOptions).then(response => response.json()).then(json => this.setState({ news: json }));
+    };
+
+
+    render() {
+        const { news } = this.state;
+        const newsList = news.length
+            ? news.map((newsItem, index) => (
+
+                <li key={index}>
+                        <div class="blog-module-item">
+                            {/* <Link to={`/details/${newsItem.uniquekey}`} target="_blank"> */}
+                                {newsItem.title}
+                            {/* </Link> */}
+                        </div>
+                </li>
+            ))
+            : '没有加载到任何新闻';
+        return (
+            <div class="blog-module shadow">
+                <Divider>{ this.props.title}</Divider>
+                    <ul class="fa-ul blog-module-ul">
+                        {newsList}
+                    </ul>
+            </div>
+        );
+    };
+
+}
--- src/js/components/pc_blog_content.js
+++ src/js/components/pc_blog_content.js
@@ -4,6 +4,7 @@ import PCArticleList from './pc_article_list'
 import PCBlogBanner from './pc_blog_banner';
 import PCBlogTips from './pc_blog_tips';
 import PCBlogAbout from './pc_blog_about';
+import PCArticleBlock from './pc_article_block';
 
 export default class PCBlogContent extends React.Component {
 
@@ -20,6 +21,9 @@ export default class PCBlogContent extends React.Component {
 						</Col>
 						<Col span={8}>
 							<PCBlogAbout />
+							<PCArticleBlock  count={8} type="top" width="100%" bordered="false" title="热文排行"/>
+							<PCArticleBlock  count={8} type="guonei" width="100%" bordered="false" title="最近分享"/>
+							<PCArticleBlock  count={8} type="yule" width="100%" bordered="false" title="友情链接"/>
 						</Col>
 					</Col>
 					<Col span={2}></Col>
